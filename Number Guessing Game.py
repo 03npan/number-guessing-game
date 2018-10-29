e6d4eb38cf48d91822027a2562540ea67f673c40
Nathan Pan <44064067+03npan@users.noreply.github.com>
2018-10-29 18:29:26 -0400

    Update Number Guessing Game.py

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -11,11 +11,19 @@ def userGuess(guess_number):
         print("Sorry, you guessed too low! Try again.")
     return guess
 
+#Creates the random number depending on what range the user would like
+def createNumber():
+    low = int(input("What would you like the lowest number to be?"))
+    high = int(input("What would you like the highest number to be?"))
+    guess_number = random.randint(low, high)
+    print("I am thinking of a number between " + str(low) + " and " + str(high) + ". Try and guess what it is!")
+    return guess_number
+
 
 def main():
-    print("Let's play a game! I am thinking of a number between 1 and 100. Try and guess what it is.")
+    print("Let's play a game! You give me two numbers, and I will come up with a number between them. Try and guess what the number is.")
     guesses = []
-    guess_number = random.randint(0, 101) #can be changed to any number
+    guess_number = createNumber()
     again = "y"
     while(again == "y"):
         guess = userGuess(guess_number)
@@ -24,7 +32,10 @@ def main():
         print("Guesses: " + strguesses)
         if guess == guess_number:
             again = input("Would you like to play again? y/n").lower()
-            guesses = []
-            guess_number = random.randint(0, 101)
+            if again == "y":
+                guesses = []
+                guess_number = createNumber()
+            else:
+                exit               
         
 main()
